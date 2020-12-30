85fe0117a4163ce511180de5799878f4b274eadc
Putri Anna <putrianna222000@gmail.com>
2020-12-30 22:25:18 +0700

    I tried bottom bar and Home page

--- lib/home.dart
+++ lib/home.dart
@@ -3,6 +3,34 @@ import 'package:flutter/material.dart';
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: SingleChildScrollView(
+        child: Container(
+          child: Column(
+            children: [
+              Container(
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: <Widget>[
+                    Container(
+                      width: 200,
+                      child: Image.asset(""),
+                    ),
+                    Container(
+                      width: 200,
+                      child: Image.asset(""),
+                    ),
+                    Container(
+                      width: 200,
+                      child: Image.asset(""),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
