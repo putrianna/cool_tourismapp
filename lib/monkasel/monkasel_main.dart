35894fa6caca210bc21f4bf804553e8646c9eb4e
cattoMeow <61348285+cattoMeow@users.noreply.github.com>
2021-01-02 16:36:07 +0700

    separating navbar from main

--- lib/monkasel/monkasel_main.dart
+++ lib/monkasel/monkasel_main.dart
@@ -1,84 +1,17 @@
-import 'package:cool_tourismapp/contactus.dart';
-import 'package:cool_tourismapp/monkasel/home.dart';
-import 'package:cool_tourismapp/monkasel/todo.dart';
 import 'package:flutter/material.dart';
+import 'navbar.dart';
 
 void main() => runApp(MyApp());
 
 /// This is the main application widget.
 class MyApp extends StatelessWidget {
-  static const String _title = 'Flutter Code Sample';
+  static const String _title = 'Cool Tourism';
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: _title,
-      home: MyStatefulWidget(),
-    );
-  }
-}
-
-/// This is the stateful widget that the main application instantiates.
-class MyStatefulWidget extends StatefulWidget {
-  MyStatefulWidget({Key key}) : super(key: key);
-
-  @override
-  _MyStatefulWidgetState createState() => _MyStatefulWidgetState();
-}
-
-/// This is the private State class that goes with MyStatefulWidget.
-class _MyStatefulWidgetState extends State<MyStatefulWidget> {
-  int _selectedIndex = 0;
-  static const TextStyle optionStyle =
-      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
-  static List<Widget> _widgetOptions = <Widget>[
-    Home(),
-    Todo(),
-    Text(
-      'Index 2: History',
-      style: optionStyle,
-    ),
-    ContactUs(),
-  ];
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: _widgetOptions.elementAt(_selectedIndex),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.explore),
-            label: 'What-To-Do',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.history),
-            label: 'History',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.info),
-            label: 'About Us',
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Color.fromARGB(255, 115, 75, 118),
-        unselectedItemColor: Colors.black54,
-        backgroundColor: Colors.white,
-        elevation: 12,
-        onTap: _onItemTapped,
-      ),
+      home: Navbar(),
     );
   }
 }
